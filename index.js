39155e347f2bc5fac0cda70536510c5e499fdf5b
Lea Moser <55796458+leamoser@users.noreply.github.com>
2023-11-15 08:54:26 +0100

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+// open the database file
+const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+    connectionStateRecovery: {}
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(new URL('./index.html', import.meta.url).pathname);
+});
+
+io.on('connection', async (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', async (msg) => {
+        console.log('message: ' + msg);
+        let result;
+        try {
+            result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (e) {
+            // TODO handle the failure
+            return;
+        }
+        // include the offset with the message
+        io.emit('chat message', msg, result.lastID);
+    });
+    if (!socket.recovered) {
+        // if the connection state recovery was not successful
+        try {
+            await db.each('SELECT id, content FROM messages WHERE id > ?',
+                [socket.handshake.auth.serverOffset || 0],
+                (_err, row) => {
+                    socket.emit('chat message', row.content, row.id);
+                }
+            )
+        } catch (e) {
+            // something went wrong
+        }
+    }
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+});
